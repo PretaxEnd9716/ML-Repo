4f6e1ee34d543330bfd342fbb7ee851fa551def7
PretaxEnd <rjdurana@gmail.com>
2022-09-11 02:17:24 -0500

    Finishing HW1

--- HW 1/rcd180001-HW1.cpp
+++ HW 1/rcd180001-HW1.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 using namespace std;
+
+void printStats(vector<double> v);
+double sum(vector<double> v);
+double mean(vector<double> v);
+double median(vector<double> v);
+double range(vector<double> v);
+double covar(vector<double> rm, vector<double> medv);
+double cor(vector<double> rm, vector<double> medv);
+double standardDev(vector<double> v);
 
 int main(int argc, char** argv)
 {
@@ -39,5 +50,107 @@ int main(int argc, char** argv)
     rm.resize(numObservations);
     medv.resize(numObservations);
 
+    cout << "Length " << rm.size() << endl;
+
+    cout << "Closing file Boston.csv" << endl;
+    filestream.close();
+
+    //Print Stats
+    cout << "\nRM Stats\n";
+    printStats(rm);
+    
+
+    cout << "\nMEDV Stats\n";
+    printStats(medv);
+
+    cout << "\nCovariance: " << covar(rm, medv) << endl;
+    cout << "Correlation: " << cor(rm, medv) << endl;
+
     return 0;
+}
+
+void printStats(vector<double> v)
+{
+    //Sorts Vector
+    sort(v.begin(), v.end());
+
+    //Obtains all needed stats
+    double s = sum(v);
+    double m = mean(v);
+    double med = median(v);
+    double r = range(v);
+
+    //Print stats
+    cout 
+    << "Sum: " << s << endl
+    << "Mean: " << m << endl
+    << "Median: " << med << endl
+    << "Range: " << r << endl;
+}
+
+double sum(vector<double> v)
+{
+    int s = 0;
+
+    for(int i = 0; i < v.size(); i++)
+    {
+        s += v.at(i);
+    }
+
+    return s;
+}
+
+double mean(vector<double> v)
+{
+    return sum(v) / v.size();
+}
+
+double median(vector<double> v)
+{
+    return v.at(v.size() / 2);
+}
+
+double range(vector<double> v)
+{
+    return v.at(v.size() - 1) - v.at(0) ;
+}
+
+double covar(vector<double> rm, vector<double> medv)
+{
+     double rm_mean = mean(rm);
+     double medv_mean = mean(medv);
+
+     double sum = 0;
+     for(int i = 0; i < rm.size(); i++)
+     {
+        double r = rm.at(i) - rm_mean;
+        double m = medv.at(i) - medv_mean;
+
+        sum += (r * m);
+     }
+
+    return sum / (rm.size() - 1);
+}
+
+double cor(vector<double> rm, vector<double> medv)
+{
+    double c = covar(rm, medv);
+    double rmStd = standardDev(rm);
+    double medStd = standardDev(medv);
+    double mult = rmStd * medStd;
+
+    return (c/mult);
+}
+
+double standardDev(vector<double> v)
+{
+    double sum = 0;
+    double m = mean(v);
+    for(int i = 0; i < v.size(); i++)
+    {
+        double diff = (v.at(i) - m);
+        sum += (diff * diff);
+    }
+
+    return sqrt(sum / v.size());
 }
